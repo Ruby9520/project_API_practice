720d27e80c852b16f27833cda2223811e39fbae5
rubykao <ruby850810@gmail.com>
2020-06-28 11:15:18 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 $(document).ready(function($){
     const app = $('#root');
-    
+    const container = $('.container');
     var request=new XMLHttpRequest();
     request.open('GET', 'https://ghibliapi.herokuapp.com/films');
     request.onload=function(){
@@ -9,6 +9,19 @@ $(document).ready(function($){
         if(request.status >=200 && request.status <400){    
             data.forEach(movie => {
                 console.log(movie.title);
+                // card ¥»¤H
+                const card= document.createElement('div');
+                card.attr('class', 'card');
+                //title
+                const h1 = document.createElement('h1');
+                //h1.text(movie.title);
+                h1.textContent=movie.title;
+                //content
+                const p = document.createElement('p');
+                movie.description= movie.description.substring(0, 300);
+                //des.text('$(movie.description)'+'...');
+                p.textContent = `${movie.description}...`;
+                container.append(card, h1, p);
             })
         }else{
             console.log('somthing goes wrong');

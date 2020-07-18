ecf86f3af0756ef9dce962b41e9acdf7a8f29ce7
rubykao <ruby850810@gmail.com>
2020-07-18 14:09:56 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -49,7 +49,7 @@ $(document).ready(function($){
         //var response = JSON.stringify(data); //array Âàjson
         //var response = JSON.stringify(Object.assign({}, data));
         console.log(typeof response);//string
-        response.each(function(movie){
+        response.forEach(function(movie){
             console.log(movie.title);
             const card= document.createElement('div');
             card.attr('class', 'card col-md-6');

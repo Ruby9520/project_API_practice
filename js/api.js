f981596bddce6fd53603b8e5e50f73ee6f6eb6bb
rubykao <ruby850810@gmail.com>
2020-07-18 22:42:22 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,39 @@
-/*$(document).ready(function($){
+$(document).ready(function($){
+    const root = $('#root');
+    const root_inner=$('#root_inner');
+    const container = $('.container');
+    $.get('https://ghibliapi.herokuapp.com/films', function(data){
+        console.log(data);
+        var response= data;
+        //var response = Object.assign({}, data);//array轉object
+        //var response = JSON.stringify(data); //array 轉json
+        console.log(typeof response);
+        response.forEach(movie => {
+            console.log(movie.title);
+            const card= document.createElement('div');
+            //attr is not a function, cause jQuery object are plain DOM elements, you can wrap it up by $()
+            $(card).attr('class', 'card col-md-6');
+            
+            const card_child=document.createElement('div');
+            $(card_child).attr('class', 'card_child');
+            //title
+            const h1 = document.createElement('h1');
+            $(h1).text(movie.title);
+            //content
+            const p = document.createElement('p');
+            movie.description= movie.description.substring(0, 300);
+            p.textContent = `${movie.description}...`;
+            const hr= document.createElement('hr')
+            card_child.append(h1, hr, p);
+            card.append(card_child);
+            root_inner.append(card);
+            //container.append(root);
+        });
+    });
+})
+
+/*original
+$(document).ready(function($){
     const root = $('#root');
     const root_inner=$('#root_inner');
     const container = $('.container');
@@ -34,41 +69,5 @@
         }
     }
     request.send();
-})*/
-
-
-
-$(document).ready(function($){
-    const root = $('#root');
-    const root_inner=$('#root_inner');
-    const container = $('.container');
-    
-    $.get('https://ghibliapi.herokuapp.com/films', function(data){
-        console.log(data);
-        var response= data;
-        //var response = Object.assign({}, data);//array轉object
-        //var response = JSON.stringify(data); //array 轉json
-        console.log(typeof response);
-        response.forEach(movie => {
-            console.log(movie.title);
-            const card= document.createElement('div');
-            //attr is not a function, cause jQuery object are plain DOM elements, you can wrap it up by $()
-            $(card).attr('class', 'card col-md-6');
-            
-            const card_child=document.createElement('div');
-            $(card_child).attr('class', 'card_child');
-            //title
-            const h1 = document.createElement('h1');
-            $(h1).text(movie.title);
-            //content
-            const p = document.createElement('p');
-            movie.description= movie.description.substring(0, 300);
-            p.textContent = `${movie.description}...`;
-            const hr= document.createElement('hr')
-            card_child.append(h1, hr, p);
-            card.append(card_child);
-            root_inner.append(card);
-            //container.append(root);
-        });
-    });
 })
+*/

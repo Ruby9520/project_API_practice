a66595628447fbbe608447eab0358efd61dc455a
Ruby9520 <ruby850810@gmail.com>
2021-02-19 23:31:58 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,13 +3,11 @@ $(document).ready(function($){
     const root_inner=$('#root_inner');
     const container = $('.container');
     $.get('https://ghibliapi.herokuapp.com/films', function(data){
-        console.log(data);
         var response= data;
-        //var response = Object.assign({}, data);//arrayÂàobject
-        //var response = JSON.stringify(data); //array Âàjson
-        console.log(typeof response);
+        //var response = Object.assign({}, data);//arrayÃ‚Ã object
+        //var response = JSON.stringify(data); //array Ã‚Ã json
         response.forEach(movie => {
-            console.log(movie.title);
+            //console.log(movie.title);
             const card= document.createElement('div');
             //attr is not a function, cause jQuery object are plain DOM elements, you can wrap it up by $()
             $(card).attr('class', 'card col-md-6');
@@ -27,47 +25,6 @@ $(document).ready(function($){
             card_child.append(h1, hr, p);
             card.append(card_child);
             root_inner.append(card);
-            //container.append(root);
         });
     });
 })
-
-/*original
-$(document).ready(function($){
-    const root = $('#root');
-    const root_inner=$('#root_inner');
-    const container = $('.container');
-    var request=new XMLHttpRequest();
-    request.open('GET', 'https://ghibliapi.herokuapp.com/films');
-    request.onload=function(){
-    //start accessing JSON data
-        var data = JSON.parse(this.response);
-        console.log(typeof data);
-        if(request.status >=200 && request.status <400){
-            data.forEach(movie => {
-                // card ¥»¤H
-                const card= document.createElement('div');
-                card.setAttribute('class', 'card col-md-6');
-                
-                const card_child=document.createElement('div');
-                card_child.setAttribute('class', 'card_child');
-                //title
-                const h1 = document.createElement('h1');
-                h1.textContent=movie.title;
-                //content
-                const p = document.createElement('p');
-                movie.description= movie.description.substring(0, 300);
-                p.textContent = `${movie.description}...`;
-                const hr= document.createElement('hr')
-                card_child.append(h1, hr, p);
-                card.append(card_child);
-                root_inner.append(card);
-                //container.append(root);
-            })
-        }else{
-            console.log('somthing goes wrong');
-        }
-    }
-    request.send();
-})
-*/
